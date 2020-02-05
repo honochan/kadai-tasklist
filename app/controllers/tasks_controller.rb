db5da3a8159e8f1260a745934d43d63f1ddfb43b
honochan <scscsic01210@gmail.com>
2020-02-05 13:10:01 +0000

    とりあえず

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,33 +4,51 @@ class TasksController < ApplicationController
   
   def index
     if logged_in?
-      @task = current_user.tasks.build  # form_with 用
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
     end
+  end
+  
+  def new
+    @task = Task.new
   end
   
   def create
     @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'メッセージを投稿しました。'
+      flash[:success] = 'タスクを投稿しました。'
       redirect_to root_url
     else
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-      flash.now[:danger] = "メッセージの投稿に失敗しました。#{current_user.id}"
+      flash.now[:danger] = "タスクの投稿に失敗しました。"
       render 'tasks/index'
+    end
+  end
+  
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Taskは更新されませんでした'
+      render :edit
     end
   end
 
   def destroy
     @task.destroy
-    flash[:success] = 'メッセージを削除しました。'
+    flash[:success] = 'Taskを削除しました。'
     redirect_back(fallback_location: root_path)
   end
   
   private
 
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:status,:content)
   end
   
   def correct_user
