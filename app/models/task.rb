2c6778b070138d2120d3263c15fa3bf114a6334e
honochan <scscsic01210@gmail.com>
2020-02-11 13:10:55 +0000

    complete

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
-  validates :status, presence: true, length: {maximum:50}
+  validates :status, presence: true, length: {maximum:11}
   validates :content, presence: true, length: {maximum:255}
 
 end
